66b37d3d4b80b43c18e1c13e1edad7da209cb5f0
David Arquero Sanjuán <9205114@alumnos.ufv.es>
2023-03-21 11:35:30 +0100

    ola

--- Hundir la flota/Barco.cs
+++ Hundir la flota/Barco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,95 @@ namespace Hundir_la_flota
             NumeroEspacios = numeroEspacios;
             Espacios = espacios;
         }
+
+
+        public string[,] PosicionarBarco(string[,] matriz)
+        {
+            bool bucle = true;
+            int opcion = 0;
+            int posicioni = 0;
+            int posicionj = 0;
+
+            while (bucle == true)
+            {
+                string texto1 = "Introduce la fila en la que se encuentra tu barco";
+                Program.Centrar(texto1);
+                posicioni = Convert.ToInt32(Console.ReadLine()) - 1;
+                string texto2 = "introduce la columna en la que se encuentra ";
+                texto2 = Program.Centrar2(texto2);
+                Console.WriteLine(texto2);
+                posicionj = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                string menu = "Introduce 1.Para colocar tu barco hacia arriba\n";
+                menu = Program.Centrar2(menu);
+                string menu2 = "Introduce 2.Para colocar tu barco hacia abajo\n";
+                menu2 = Program.Centrar2(menu2);
+                string menu3 = "Introduce 3.Para colocar tu barco hacia la izquierda\n";
+                menu3 = Program.Centrar2(menu3);
+                string menu4 = "Introduce 4.Para colocar tu barco hacia la derecha\n";
+                menu4 = Program.Centrar2(menu4);
+
+                menu = menu + menu2 + menu3 + menu4;
+                Console.WriteLine(menu);
+                opcion = Convert.ToInt32(Console.ReadLine());
+                bool bucle = ComprobarTablero(opcion, posicioni, posicionj);
+                if (bucle == true)
+                {
+                    string error = "Tu barco se sale del tablero, decide otra vez hacia donde colocarlo";
+                    error = Program.Centrar2(error);
+                }
+                else
+                {
+                    ComprobarEspacioLibre(opcion, posicioni, posicionj, matriz);
+                    bool libre;
+                }
+            }
+        }
+
+        public bool ComprobarTablero(int opcion, int posicioni, int posicionj)
+        {
+            bool bucle;
+            if ((opcion == 1 && posicioni >= NumeroEspacios - 1) || opcion != 1)
+            {
+                if ((opcion == 2 && posicioni <= 11 - NumeroEspacios - 1) || opcion != 2)
+                {
+                    if ((opcion == 3 && posicionj >= NumeroEspacios - 1) || opcion != 3)
+                    {
+                        if ((opcion == 4 && posicionj <= 11 - NumeroEspacios - 1) || opcion != 4)
+                        {
+                            bucle = false;
+                        }
+                        else { bucle = true; }
+                    }
+                    else { bucle = true; }
+                }
+                else { bucle = true; }
+            }
+            else { bucle = true; }
+            return bucle;
+        }
+
+
+        public bool ComprobarEspacioLibre(int opcion, int posicioni, int posicionj, string[,] matriz)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    int libre = 1;
+                    for (int i = 0; i < NumeroEspacios; i++)
+                    {
+                        if (matriz[posicioni - i, posicionj] == "X")
+                        {
+                            libre = 2;
+                        }
+                        else if (matriz[posicioni - i, posicionj] == Convert.ToChar(1).ToString())
+                        {
+                            libre = 2;
+                        }
+                    }
+            }
+
+
         public string[,] PosicionarBarco(string[,] matriz)
         {
             bool bucle = true;
