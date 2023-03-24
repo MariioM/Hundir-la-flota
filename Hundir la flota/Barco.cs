79814a40737598ad941412092663c81b04f279aa
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2023-03-24 10:16:28 +0100

    Cambiado

--- Hundir la flota/Barco.cs
+++ Hundir la flota/Barco.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hundir_la_flota
 {
-    abstract class Barco
+    internal class Barco
     {
         //Atributos
         public int NumeroEspacios { get; set; }
